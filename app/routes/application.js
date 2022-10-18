e82bc4b73fde6dfbab88a359f2cd02135075c595
Thaya <selvathayalan.r@gmail.com>
2022-10-19 00:21:56 +0530

    model used to test whether the api req is working without any error.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
-
+    async model(){
+        let res = await fetch("https://e-cards-api-production.up.railway.app/menuCards");
+        let data = await res.json();
+        console.log("res: ",data);
+        return data;
+    }
 }
