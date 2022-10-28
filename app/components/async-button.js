b2814f64c0653d670e87db0dbd36d84115d831c8
Thaya <selvathayalan.r@gmail.com>
2022-10-29 00:31:20 +0530

    Created a Async button component using task to use for creating cards and used that in the menu-card creation form.

---
+++ app/components/async-button.js
@@ -0,0 +1,28 @@
+import Component from '@ember/component';
+import { task } from "ember-concurrency";
+
+export default class AsyncButtonComponent extends Component {
+    tagName = "button";
+    classNames = ["async-btn","e-card-btn"];
+    classNameBindings = ["isClicked:disabled", "isError:error"]
+
+    //args...
+    initialButtonText = "";
+    waitingButtonText = "";
+
+    //properties...
+    isClicked = false;
+    isError = false;
+    errorStack = "";
+
+    click(){
+        this.onClickTask.perform();
+    }
+
+    @task
+    *onClickTask(){
+        yield this.onClick().then(
+            () => this.setProperties({errorStack: "", isError: false, isClicked: true}),
+            (err) => this.setProperties({errorStack: err, isError: true}));
+    }
+}
--- app/controllers/menu-card.js
+++ app/controllers/menu-card.js
@@ -1,17 +1,27 @@
 import Controller from '@ember/controller';
-import { action } from "@ember/object"
+import { action } from "@ember/object";
 
 export default class MenuCardController extends Controller {
-    hotelName = '';
-    menuItems = undefined;
-  
-    init() {
-      super.init(...arguments);
-      this.menuItems = [{ name: '', quantity: '', price: 0 }];
-    }
+  hotelName = '';
+  menuItems = undefined;
 
-    @action
-    addNewItem(){
-        this.menuItems.pushObject({ name: '', quantity: '', price: 0 });
-    }
+  init() {
+    super.init(...arguments);
+    this.menuItems = [{ name: '', quantity: '', price: 0 }];
+  }
+
+  @action
+  addNewItem() {
+    this.menuItems.pushObject({ name: '', quantity: '', price: 0 });
+  }
+
+  @action
+  async onCreateMenu(){
+    //To testing the async button we are used this stuff. Need to replace with actual createMenu functionality.
+    return new Promise((resolve, error)=>  {
+      setTimeout(()=>{
+        return resolve("hai");
+      }, 1000);
+    });
+  }
 }
