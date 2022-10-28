8dfa3a2bd07198385be2837bd77395a93d34aa65
Thaya <selvathayalan.r@gmail.com>
2022-10-28 20:11:24 +0530

    Commented the home route model hook as the api is down for now.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
-    async model(){
-        let res = await fetch("https://e-cards-api-production.up.railway.app/menuCards");
-        let data = await res.json();
-        console.log("res: ",data);
-        return data;
-    }
+  // async model() {
+  //   let res = await fetch(
+  //     'https://e-cards-api-production.up.railway.app/menuCards'
+  //   );
+  //   let data = await res.json();
+  //   console.log('res: ', data);
+  //   return data;
+  // }
 }
