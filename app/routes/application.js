9b972454818522951d18dc5ef43560056ccc938a
Thaya <selvathayalan.r@gmail.com>
2022-11-03 00:55:41 +0530

    Replaced fetch with request handler's get method.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,9 +1,12 @@
 import Route from '@ember/routing/route';
-import environment from 'e-cards/config/environment';
-
+import { inject } from '@ember/service';
 export default class ApplicationRoute extends Route {
+
+   @inject
+   requestHandler;
+   
    async model() {
-     let res = await fetch(`${environment.API_URL}menuCards`).then(resp =>{
+     let res = await this.requestHandler.getRequest("menuCards").then(resp =>{
       return resp.json();
      }).catch(err=>{
       console.log("err: ",err);
